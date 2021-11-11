a623a56d5e5e93c4924fec2fac07040a2b4bd026
pascuord <pascuord@gmail.com>
2021-11-11 12:29:21 +0100

    Subida proyecto finalizado

---
+++ dist/index.9beceafd.js
@@ -0,0 +1,13 @@
+var i = 0;
+var txt = 'Moon Trip'; /* The text */ 
+var speed = 200; /* The speed/duration of the effect in milliseconds */ 
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("title").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+typeWriter();
+
+//# sourceMappingURL=index.9beceafd.js.map
